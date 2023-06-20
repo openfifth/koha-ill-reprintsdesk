bd37699dd28128337eb040acd800b2516190ce8e
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-06-20 15:42:06 +0000

    Add illrequest_id to backend API call, and then to plugin API call

--- Base.pm
+++ Base.pm
@@ -677,7 +677,7 @@ sub create_request {
     }
 
     # Make the request with Reprints Desk via the koha-plugin-reprintsdesk API
-    my $response = $self->{_api}->Order_PlaceOrder2( $metadata, $submission->borrowernumber );
+    my $response = $self->{_api}->Order_PlaceOrder2( $metadata, $submission->borrowernumber, $submission->illrequest_id );
 
     # If the call to Reprints Desk was successful,
     # add the Reprints Desk request ID to our submission's metadata
