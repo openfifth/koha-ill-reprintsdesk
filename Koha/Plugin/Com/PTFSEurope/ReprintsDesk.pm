ee74a8aabe3f679ed02b0c4a351dd10c40d32f1b
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2025-08-12 10:33:17 +0000

    version bump

--- Koha/Plugin/Com/PTFSEurope/ReprintsDesk.pm
+++ Koha/Plugin/Com/PTFSEurope/ReprintsDesk.pm
@@ -39,7 +39,7 @@ use Koha::Plugin::Com::PTFSEurope::ReprintsDesk::Processor::EnqueueNotices;
 use Koha::Libraries;
 use Koha::Patrons;
 
-our $VERSION = "3.0.3";
+our $VERSION = "3.0.4";
 
 our $metadata = {
     name            => 'ReprintsDesk',
