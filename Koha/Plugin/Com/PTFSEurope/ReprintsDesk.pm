782bd53aa83d7bf5dd40f464091daea74a8c99cd
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2025-08-18 10:09:02 +0000

    version bump

--- Koha/Plugin/Com/PTFSEurope/ReprintsDesk.pm
+++ Koha/Plugin/Com/PTFSEurope/ReprintsDesk.pm
@@ -39,13 +39,13 @@ use Koha::Plugin::Com::PTFSEurope::ReprintsDesk::Processor::EnqueueNotices;
 use Koha::Libraries;
 use Koha::Patrons;
 
-our $VERSION = "3.1.0";
+our $VERSION = "3.2.0";
 
 our $metadata = {
     name            => 'ReprintsDesk',
     author          => 'Open Fifth',
     date_authored   => '2022-04-26',
-    date_updated    => "2025-08-13",
+    date_updated    => "2025-08-18",
     minimum_version => '24.05.00.000',
     maximum_version => undef,
     version         => $VERSION,
