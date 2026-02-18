46dadb4bde84fab84cf1382ef3fa9593afd7c041
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2026-02-18 10:30:21 -0100

    Reimplement not being able to cancel a 'REQ' request
    Utilize status graph, not JS .hide()

--- Koha/Plugin/Com/PTFSEurope/ReprintsDesk.pm
+++ Koha/Plugin/Com/PTFSEurope/ReprintsDesk.pm
@@ -1236,16 +1236,27 @@ sub status_graph {
         # Override REQ so we can rename the button
         # Talk about a sledgehammer to crack a nut
         REQ => {
-            prev_actions   => [ 'READY', 'REQREV', 'QUEUED' ],
+            prev_actions   => [ 'READY', 'QUEUED' ],
             id             => 'REQ',
             name           => 'Requested',
             ui_method_name => 'Request from Reprints Desk',
             method         => 'confirm',
-            next_actions   => [ 'REQREV', 'CHK' ],
+            next_actions   => [ 'CHK' ],
             ui_method_icon => 'fa-check',
         },
+
+        # Override REQREV so we can effectivelly disable it
+        REQREV => {
+            prev_actions   => [ ],
+            id             => 'REQREV',
+            name           => 'Request reverted',
+            ui_method_name => 'Revert request',
+            method         => 'cancel',
+            next_actions   => [ ],
+            ui_method_icon => 'fa-times',
+            },
         MIG => {
-            prev_actions   => [ 'NEW', 'REQ', 'GENREQ', 'REQREV', 'STANDBY', 'QUEUED' ],
+            prev_actions   => [ 'NEW', 'REQ', 'GENREQ', 'STANDBY', 'QUEUED' ],
             id             => 'MIG',
             name           => 'Switched provider',
             ui_method_name => 'Switch provider',
